b8254c054045e595695b378d7b6b50516a8a9530
annacwu <annacwu04@gmail.com>
2023-10-25 15:03:28 -0700

    Initial Commit

---
+++ Checkity/CheckityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckityApp.swift
+//  Checkity
+//
+//  Created by Anna Wu on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
