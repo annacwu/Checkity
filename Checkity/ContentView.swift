92f84fa3c0e93fa1dc3c60c66a1a22c53e11b0cf
annacwu <annacwu04@gmail.com>
2023-10-30 11:38:55 -0700

    added navigation stack with forms for primary functionality

--- Checkity/ContentView.swift
+++ Checkity/ContentView.swift
@@ -8,30 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
-    //@State var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    let tipPercentages = [10,15,20,25,0]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let total = tipValue + checkAmount
+        let amountPerPerson = total / peopleCount
+        
+        return amountPerPerson
+    }
     var body: some View {
-//        VStack {
-//            Text("Split!")
-//        }
-//        .padding()
         NavigationStack{
-            Form {
+            Form{
+                Section("Enter check total and number of people:"){
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<50){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section("Select Tip Amount:"){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
                 Section{
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)")
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Checkity")
         }
-        
-//        Button("Tap Count: \(tapCount)") {
-//            tapCount += 1
-//        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+// PRACTICE!!!
+//
+//import SwiftUI
+//
+//struct ContentView: View {
+//    //@State var tapCount = 0
+//    @State private var name = ""
+//    let students = ["Harry", "Hermione", "Ron"]
+//    @State private var selectedStudent = "Harry"
+//    
+//    var body: some View {
+//        //        VStack {
+//        //            Text("Split!")
+//        //        }
+//        //        .padding()
+//        NavigationStack{
+//            Form {
+//                //                Section{
+//                //                    TextField("Enter your name", text: $name)
+//                //                    Text("Your name is \(name)")
+//                //                }
+//                Picker("Select your student", selection: $selectedStudent) {
+//                    ForEach(students, id: \.self) {
+//                        Text($0)
+//                    }
+//                }
+//                .navigationTitle("SwiftUI")
+//                .navigationBarTitleDisplayMode(.inline)
+//            }
+//            
+//            //        Button("Tap Count: \(tapCount)") {
+//            //            tapCount += 1
+//            //        }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
